a1316b686b132abec0c5184a17d54c55785711f3
keysorbawah <keysorbawah@gmail.com>
2024-06-12 14:34:36 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# smpngentot
-smpngentot
+A live demo of the project is available at [https://smpngentot.pages.dev](https://smpngentot.pages.dev/).
+Fork the [repository](https://github.com/keysorbawah/viralngewe) on GitHub.
---
+++ app/layout.tsx
@@ -0,0 +1,97 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+import { SITENAME } from "@/lib/constants";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: `${SITENAME} - Video Bokep Indo Viral Terbaru`,
+    description: `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+    metadataBase: new URL("https://smpngentot.pages.dev"),
+    alternates: {
+        canonical: `/`,
+    },
+    openGraph: {
+        title: `${SITENAME} - Video Bokep Indo Jepang Barat Viral Terbaru`,
+        description: `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+        url: `/`,
+        type: `website`,
+    },
+    verification: {
+        google: 'CTrPfxsT9xBsLtRDZ42AK8JKcCS_7rqpOEl8RYD5mWQ',
+        yandex: 'dc8a433b96c1de22',
+    },
+};
+
+export const runtime = "edge";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    const jsonLd = {
+        "@context": "https://schema.org",
+        "@type": "Organization",
+        "name": `${SITENAME}`,
+        "description": `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+        "logo": "https://smpngentot.pages.dev/favicon.ico",
+        "url": "https://smpngentot.pages.dev",
+        "founder":{
+            "@type":"Person",
+            "name":"admin",
+            "url":"https://smpngentot.pages.dev"
+            },
+            "foundingDate":"2024-06-11"
+        }
+        const jsonLd1 = {
+            "@context": "https://schema.org",
+        "@type": "WebPage",
+        "name": `${SITENAME}`,
+        "url": "https://smpngentot.pages.dev",
+        "description": `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+        "image": "https://smpngentot.pages.dev/favicon.ico",
+        "potentialAction":{
+            "@type":"ReadAction",
+            "target":"https://smpngentot.pages.dev"}
+        }
+        const jsonLd2 = {
+            "@context": "https://schema.org",
+        "@type": "WebSite",
+        "name": `${SITENAME}`,
+        "url": "https://smpngentot.pages.dev",
+        "description": `${SITENAME} Video Bokep Indo Bocil Jepang Ngentot Jav Jilbab Smp Barat Mama Viral Sma Korea Tante China Live Paksa Bokep Abg Terbaru`,
+        "potentialAction": { 
+        "@type": "SearchAction", 
+          "target": "https://smpngentot.pages.dev/?q={search_term}", 
+            "query-input": "required name=search_term"}
+        }
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body className={font.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="system"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd1) }}
+        />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd2) }}
+        />
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
+}
